a6aea4df26432d443e46aa036192106d0a1630bf
Calopsia <pseba20@gmail.com>
2017-02-01 16:48:01 -0300

    .

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
 
@@ -45,13 +45,13 @@
 
     <!-- Custom Fonts -->
     <link href="framework/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="framework/fonts/css.css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="framework/js/html5shiv.js"></script>
+    <script src="framework/js/respond.min.js"></script>
     <![endif]-->
 
 </head>
@@ -1128,8 +1128,8 @@ if($tipo==1) {
                 <p>Argentina
                     <br>San Juan</p>
                 <ul class="list-unstyled">
-                    <li><i class="fa fa-phone fa-fw"></i> (+54 0264) 4305619 / 4305575</li>
-                    <li><i class="fa fa-envelope-o fa-fw"></i> <a href="mailto:msal@sanjuan.gov.ar">Area de Informática - Ministerio de Salud Pública</a>
+                    <li><i class="fa fa-phone fa-fw"></i> (+54 0264) 4305619 / 4305575 - msal.sanjuan@gmail.com</li>
+                    <li><i class="fa fa-envelope-o fa-fw"></i> <a href="mailto:msal.sanjuan@gmail.com">Area de Informática - Ministerio de Salud Pública</a>
                     </li>
                 </ul>
                 <br>
